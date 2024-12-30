88ff61c4cd7ae979c986da84696065130665cc01
Yannik Sacherer <A1DF6EF@jwpmx2kh4d.koeln.ads.risnet.de>
2024-12-30 10:43:32 +0100

    solved 3th lesson and started 4th lesson

--- lessons/3_lesson/main.py
+++ lessons/3_lesson/main.py
@@ -13,6 +13,7 @@ def github_stargazers():
     )
 
     for page in client.paginate("repos/dlt-hub/dlt/stargazers"):
+        print(f"next page: {page}\n")
         yield page
 
 
@@ -27,3 +28,32 @@ print(load_info)
 
 # explore loaded data
 pipeline.dataset(dataset_type="default").github_stargazers.df()
+
+
+"""
+Exercise 1: Pagination with RESTClient
+
+Explore the cells above and answer the question below.
+Question
+
+What type of pagination we use for GitHub API?
+
+If a paginator is not specified, the paginate() method will attempt to automatically 
+detect the pagination mechanism used by the API. 
+If the API uses a standard pagination mechanism like having a next link in the response's headers or JSON body,
+the paginate() method will handle this automatically. Otherwise, you can specify a paginator object explicitly 
+or implement a custom paginator.
+
+In this case it is HeaderLinkPaginator
+"""
+
+
+"""
+different pagination strategy supervised by dlt:
+
+• JSONLinkPaginator - link to the next page is included in the JSON response.
+• HeaderLinkPaginator - link to the next page is included in the response headers.
+• OffsetPaginator - pagination based on offset and limit query parameters.
+• PageNumberPaginator - pagination based on page numbers.
+• JSONResponseCursorPaginator - pagination based on a cursor in the JSON response.
+"""
